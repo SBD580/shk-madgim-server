76a54cbc8c1f8ad53510ebd231f94aad4f5e56bc
Royi Freifeld <freifeld.royi@gmail.com>
2016-07-09 02:01:42 +0300

    - updated index.js - bound /resources/aircrafts, /resources/airlines & /resources/airports to rest get function  - introduced sampler.js - sampling the data from planefinder & flight radar
     Basically this can be a good start to inject data to the client

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ var args = process.argv.slice(2);
 
 // get client location
 var clientLocation = args[0];
-if(!clientLocation){
-	console.error('usage: index.js client-location [port]');
-	process.exit();
+if (!clientLocation) {
+    console.error('usage: index.js client-location [port]');
+    process.exit();
 }
 
 // get server port
@@ -18,8 +18,23 @@ var app = express();
 app.use(express.static(clientLocation));
 
 // serve static files
-app.use('/static',express.static(__dirname+'/static'));
+app.use('/static', express.static(__dirname + '/static'));
 
-app.listen(port,function(){
-	console.log('Server is up and running. Listening on port '+port);
+app.listen(port, function () {
+    console.log('Server is up and running. Listening on port ' + port);
+});
+
+// rest handling for data sniffer
+var sampler = require('./sampler/sampler');
+
+app.get('/resources/aircrafts', function (req, res) {
+    sampler.aircrafts(res);
+});
+
+app.get('/resources/airlines', function (req, res) {
+    sampler.airlines(res);
+});
+
+app.get('/resources/airports', function (req, res) {
+    sampler.airports(res);
 });
