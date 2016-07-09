0634c019e720ae91d12588611f458081b1b4b6cf
Shay Ben David <Shay Ben David>
2016-07-10 00:35:08 +0300

    no message

--- src/index.js
+++ src/index.js
@@ -17,31 +17,35 @@ var port = args[1] || 8888;
 
 // git hooks support
 gith({
-	ref: 'refs/heads/master'
-}).on('all',function(payload){
-	console.info('Receive post-update event, pulling...');
+    ref: 'refs/heads/master'
+}).on('all', function (payload) {
+    console.info('Receive post-update event, pulling...');
 
-	if(payload.repository=='SBD580/shk-madgim-client') {
-		gitpull(clientLocation, function (err, out) {
-			if (err)
-				console.error('failed pulling directory ' + clientLocation, err, out);
-			else
-				console.info('pulled directory ' + clientLocation + ' successfully');
-		});
-		exec('npm install', {cwd: clientLocation});
-	}
+    if (payload.repository == 'SBD580/shk-madgim-client') {
+        gitpull(clientLocation, function (err, out) {
+            if (err) {
+                console.error('failed pulling directory ' + clientLocation, err, out);
+            } else {
+                console.info('pulled directory ' + clientLocation + ' successfully');
+                console.info('installing client...');
+                exec('npm install', {cwd: clientLocation});
+            }
+        });
+    }
 
-	if(payload.repository=='SBD580/shk-madgim-server') {
-		gitpull(__dirname, function (err, out) {
-			if (err)
-				console.error('failed pulling directory ' + __dirname, err, out);
-			else
-				console.info('pulled directory ' + __dirname + ' successfully');
-		});
-		exec('npm install', {cwd: __dirname},function(){
-			process.exit(0);
-		});
-	}
+    if (payload.repository == 'SBD580/shk-madgim-server') {
+        gitpull(__dirname, function (err, out) {
+            if (err) {
+                console.error('failed pulling directory ' + __dirname, err, out);
+            } else {
+                console.info('pulled directory ' + __dirname + ' successfully');
+                console.info('installing app...');
+                exec('npm install', {cwd: __dirname},function(){
+                    process.exit(0);
+                });
+            }
+        });
+    }
 });
 
 var app = express();
